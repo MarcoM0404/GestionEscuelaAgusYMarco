edc8d3b45fb22708314d1af843f8a2fa24c62938
curso-git2025 <induccion3projectsconsulting@gmail.com>
2025-07-22 11:44:05 -0300

    refactor(): iconos agregados en ProfessorProfileView

--- src/main/java/com/example/app/base/ui/view/ProfessorProfileView.java
+++ src/main/java/com/example/app/base/ui/view/ProfessorProfileView.java
@@ -30,6 +30,7 @@ public class ProfessorProfileView extends VerticalLayout {
     public ProfessorProfileView(ProfessorService profService,
                                 PersonService personService,
                                 AddressService addressService) {
+
         this.profService    = profService;
         this.personService  = personService;
         this.addressService = addressService;
@@ -41,7 +42,6 @@ public class ProfessorProfileView extends VerticalLayout {
         }
 
         setSizeFull();
-
 
         H2 header = new H2();
         header.add(new Icon(VaadinIcon.USER), new Span(" Mi Perfil"));
@@ -55,13 +55,28 @@ public class ProfessorProfileView extends VerticalLayout {
             binder = new Binder<>(Professor.class);
 
             TextField   name    = new TextField("Nombre");
+            name.setPrefixComponent(new Icon(VaadinIcon.USER));
+
             TextField   email   = new TextField("Email");
+            email.setPrefixComponent(new Icon(VaadinIcon.ENVELOPE));
+
             TextField   phone   = new TextField("Teléfono");
+            phone.setPrefixComponent(new Icon(VaadinIcon.PHONE));
+
             NumberField salary  = new NumberField("Salario");
+            salary.setPrefixComponent(new Icon(VaadinIcon.MONEY));
+
             TextField   street  = new TextField("Calle");
+            street.setPrefixComponent(new Icon(VaadinIcon.ROAD));
+
             TextField   city    = new TextField("Ciudad");
+            city.setPrefixComponent(new Icon(VaadinIcon.BUILDING));
+
             TextField   state   = new TextField("Provincia");
+            state.setPrefixComponent(new Icon(VaadinIcon.FLAG));
+
             TextField   country = new TextField("País");
+            country.setPrefixComponent(new Icon(VaadinIcon.GLOBE));
 
             binder.forField(name)
                   .asRequired("Requerido")
@@ -96,7 +111,8 @@ public class ProfessorProfileView extends VerticalLayout {
                     addressService.save(me.getAddress());
                     personService.save(me);
                     profService.save(me);
-                    Notification.show("Perfil guardado", 1500, Notification.Position.BOTTOM_START);
+                    Notification.show("Perfil guardado",
+                                      1500, Notification.Position.BOTTOM_START);
                 }
             });
             save.setIcon(new Icon(VaadinIcon.CHECK));
