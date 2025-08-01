7e3fb1abc955e565151161f27a671e199bd4ed06
MarcoM0404 <marco.marrazzo04@gmail.com>
2025-08-01 09:31:13 -0300

    refactor(domain) Modifica User.java

--- src/main/java/com/example/app/base/domain/User.java
+++ src/main/java/com/example/app/base/domain/User.java
@@ -1,13 +1,13 @@
 package com.example.app.base.domain;
 
+
+import com.example.app.security.AppRoles;
+
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
-public class User {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class User extends AbstractEntity {
 
     @Column(nullable = false, unique = true, length = 50)
     private String username;
@@ -17,55 +17,28 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
-    private Role role;
+    private AppRoles role;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private Person person;
 
     public User() {}
 
-    public User(String username, String password, Role role) {
+    public User(String username, String password, AppRoles role) {
         this.username = username;
         this.password = password;
         this.role     = role;
     }
 
-    public Long getId() {
-        return this.id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-	public String getUsername() {
-		return username;
-	}
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public AppRoles getRole() { return role; }
+    public void setRole(AppRoles role) { this.role = role; }
 
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Role getRole() {
-		return role;
-	}
-
-	public void setRole(Role role) {
-		this.role = role;
-	}
-
-	public Person getPerson() {
-		return person;
-	}
-
-	public void setPerson(Person person) {
-		this.person = person;
-	}
+    public Person getPerson() { return person; }
+    public void setPerson(Person person) { this.person = person; }
 }
