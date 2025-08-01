62c836c01fe18af0c0d7fe3773f67da8295c105d
MarcoM0404 <marco.marrazzo04@gmail.com>
2025-08-01 09:32:25 -0300

    refactor(repository) Modifica UserRepository.java

--- src/main/java/com/example/app/base/repository/UserRepository.java
+++ src/main/java/com/example/app/base/repository/UserRepository.java
@@ -2,9 +2,8 @@ package com.example.app.base.repository;
 
 import com.example.app.base.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
