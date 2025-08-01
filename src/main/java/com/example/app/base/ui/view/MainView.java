3db8c7c6d149a0c2664e736edbb2813d0a480cce
MarcoM0404 <marco.marrazzo04@gmail.com>
2025-08-01 10:47:38 -0300

    refactor(view) Modificar clase MainView.java

--- src/main/java/com/example/app/base/ui/view/MainView.java
+++ src/main/java/com/example/app/base/ui/view/MainView.java
@@ -8,14 +8,12 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 import jakarta.annotation.security.PermitAll;
 
-/**
- * This view shows up when a user navigates to the root ('/') of the application.
- */
+
 @Route
 @PermitAll // When security is enabled, allow all authenticated users
 public final class MainView extends Main {
 
-    // TODO Replace with your own main view.
+
 
     MainView() {
         addClassName(LumoUtility.Padding.MEDIUM);
@@ -23,9 +21,7 @@ public final class MainView extends Main {
         add(new Div("Please select a view from the menu on the left."));
     }
 
-    /**
-     * Navigates to the main view.
-     */
+
     public static void showMainView() {
         UI.getCurrent().navigate(MainView.class);
     }
